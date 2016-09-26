41e2b8e94fffa2822812f8512d599963cc541fd7
Louis Phan <sutrix.tu.phan@sutrixsolutions.com>
2016-09-26 15:30:58 +0700

    --- Add more and delete ---

---
+++ src/com/coursework/com/coursework/domain/Kiddy.java
@@ -0,0 +1,12 @@
+package com.coursework.com.coursework.domain;
+
+public class Kiddy {
+    private Integer id;
+    private String activityName;
+    private String location;
+    private String date;
+    private String time;
+    private String reporterName;
+
+
+}

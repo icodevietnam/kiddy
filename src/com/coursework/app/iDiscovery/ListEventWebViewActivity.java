b0b999d3391ac2ddd658e95f81b28c8507bd2d5b
Louis <pttu@sqli.com>
2017-04-20 05:35:13 +0200

    aaaa

--- src/com/coursework/app/iDiscovery/ListEventWebViewActivity.java
+++ src/com/coursework/app/iDiscovery/ListEventWebViewActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.webkit.WebChromeClient;
 
 import com.coursework.app.R;
+import com.coursework.com.coursework.domain.iDiscovery;
+import com.coursework.helper.DBHelper;
+import com.google.gson.Gson;
 
 import org.apache.cordova.CordovaActivity;
 import org.apache.cordova.CordovaWebView;
@@ -13,13 +16,21 @@ import org.apache.cordova.CordovaWebViewImpl;
 import org.apache.cordova.engine.SystemWebView;
 import org.apache.cordova.engine.SystemWebViewEngine;
 
+import java.util.List;
+
 public class ListEventWebViewActivity extends CordovaActivity {
+
+    private DBHelper myDb;
+
+    String json;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
         super.init();
+        myDb = new DBHelper(this);
+        List<iDiscovery> list = myDb.getAlliDiscoveries();
         launchUrl = "file:///android_asset/www/listEvent.html";
         loadUrl(launchUrl);
     }
